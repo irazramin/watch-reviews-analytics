04475ba758f73db03449fbf04cdbaf61a9f403d5
irazramin <irazramin@gmail.com>
2022-04-05 00:54:28 +0600

    blog parts 2nd qus added

--- src/components/AllReviews/AllReviews.js
+++ src/components/AllReviews/AllReviews.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import useFetch from '../hooks/useFatch';
+import useFetch from '../hooks/useFetch';
 import Review from '../Review/Review';
 
 const AllReviews = () => {
-    const [watchs,setWatchs] = useFetch();
+    const [reviews,setReviews] = useFetch();
   return (
     <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-7'>
-      {watchs.map(
+      {reviews.map(
         (review, idx) =>  <Review key={review.id}  review={review} />
       )}
     </div>
